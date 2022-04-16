c1ebb2e996dfa3b846a34d9bb096217c178e6b83
farabi1 <rashidfarabi@gmail.com>
2022-04-17 02:17:11 +0600

    Sample component Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Tenth Assignment</h1>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
