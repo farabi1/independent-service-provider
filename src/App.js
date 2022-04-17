18285ad6468c02925985a47deeacd32767daa795
farabi1 <rashidfarabi@gmail.com>
2022-04-17 12:59:11 +0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Tenth Assignment</h1>
+      <Button variant="danger" >Button</Button>
       <Home></Home>
       <Footer></Footer>
     </div>
