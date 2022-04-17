5e43cbacb069e7befed28429abef8fa103bad6bf
farabi1 <rashidfarabi@gmail.com>
2022-04-17 13:07:45 +0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
-import { Button } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Tenth Assignment</h1>
-      <Button variant="danger" >Button</Button>
-      <Home></Home>
-      <Footer></Footer>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/footer" element={<Footer></Footer>}></Route>
+      </Routes>
     </div>
   );
 }
